0ea37ab7207ef070ec9d8c6bb332befee52a87f8
Kholish Nurfajri <kholisnurfajri1@gmail.com>
2022-05-20 11:38:08 +0700

    first commit

--- index.js
+++ index.js
@@ -89,11 +89,12 @@ function dataProject(data){
 function renderPage(){
 
     let cardProject = document.getElementById("grid");
-    cardProject.innerHTML = "";
+    // cardProject.innerHTML += "";
     dataCard.forEach((data,index)=>{
-        cardProject.innerHTML +=`<div id="grid">
+        cardProject.innerHTML +=`
+        <div id="grid">
         <div id="cards">
-        <div class="div-up" ><a href="detail-project.html" onclick="dataProject(${data})" class="card-grid">
+        <div class="div-up" ><a href="detail-project.html" onclick="dataProject(${index})" class="card-grid">
         <img src="${data.image}"</a>
         </div>
         <div class="div-down">
@@ -121,7 +122,7 @@ function renderPageDetail(){
 
 
     dataCard.forEach((data,index) => {
-        projectDetail += `<h1>${data.title}</h1>
+        projectDetail.innerHTML += `<h1>${data.title}</h1>
                                     <div class="content-detail">
                                         <img src="${data.image}" class="head-detail-image" alt="">
                                     <div class="r">
